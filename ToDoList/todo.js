db6ebcef6cf309cd311ebd4617f47e5fb6241997
Justine Ellerbe <dom16006@byui.edu>
2020-03-06 14:33:04 -0500

    commit

--- ToDoList/todo.js
+++ ToDoList/todo.js
@@ -1,19 +1,18 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
+var myToDoList = document.getElementsByTagName("LI");
 var i;
-for (i = 0; i < myNodelist.length; i++) {
+for (i = 0; i < myToDoList.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+  myToDoList[i].appendChild(span);
 }
 
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
+  close[i].onclick = function() {
     var div = this.parentElement;
     div.style.display = "none";
   }
@@ -21,20 +20,20 @@ for (i = 0; i < close.length; i++) {
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
-list.addEventListener('click', function (ev) {
+list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
 }, false);
 
 // Create a new list item when clicking on the "Add" button
-function newElement() {
+function add() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("Can't be blank.");
+    alert("You must write something!");
   } else {
     document.getElementById("myList").appendChild(li);
   }
@@ -47,9 +46,34 @@ function newElement() {
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
+    close[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
+    }
+  }
+
+}
+
+function displayAll() {
+  let todo = document.getElementById('myList');
+}
+
+function displayRemaining() {
+  let todo = document.getElementById('myList');
+  let list = todo.getElementsByTagName('LI');
+  for(let i = 0; i < list.length; i++) {
+    if(list[i].children[0].className != 'remaining') {
+      list[i].style.display='none';
+    }
+  }
+}
+
+function displayCompleted() {
+  let todo = document.getElementById('todos');
+  let list = todo.getElementsByTagName('li');
+  for(let i = 0; i < list.length; i++) {
+    if(list[i].children[0].className != 'remaining completed') {
+      list[i].style.display='none';
     }
   }
 }
